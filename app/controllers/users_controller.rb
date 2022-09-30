7a30bc3f5a205c09370a28a4bc7709a77e1f0e2d
Carlos K Villanueva <carloskvillanueva@gmail.com>
2022-09-30 18:17:27 -0300

    Added Sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
+	
+	def index
+		@users = User.paginate(page: params[:page], per_page: 5)
+	end
+	
 	def new
 		@user = User.new
+	end
+	
+	def show
+		@user     = User.find(params[:id])
+		@articles = @user.articles.paginate(page: params[:page], per_page: 5)
 	end
 	
 	def create
@@ -21,7 +31,7 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 		if @user.update(user_params)
 			flash[:notice] = "Updated your profile successfully"
-			redirect_to articles_path
+			redirect_to @user
 		end
 	end
 	
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
-1
